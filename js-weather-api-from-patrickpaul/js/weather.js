9302dcc66eac64054474b7584979c7c0dd0345e8
Noah Jonah <noahjonah705@gmail.com>
2024-11-03 14:06:58 +0300

    Fixed responsiveness, layouts and looks.
    Created media queries for most screens from small mobile devices to large 4K display.
    Added the weather forecast advices for the current city weather status but javascript functionality are not added yet.
    Refactored javascript codes for easy maintainability.

--- js-weather-api-from-patrickpaul/js/weather.js
+++ js-weather-api-from-patrickpaul/js/weather.js
@@ -6,67 +6,72 @@ function FetchWeatherData(city, api_key) {
     this.api_key = api_key;
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric`;
 
-    this.fetch_weather = function () {
+    this.fetch_weather = async function () {
         let icons = [];
         let days = [];
         let high_temps = [];
         let low_temps = [];
 
-        return fetch(url).then(response => {
-            if (!response.ok) {
-                throw new Error(`City not found. HTTP status: ${response.status}`);
-            }
-            return response.json();
-        }).then(function (weather_data) {
-            if (weather_data.cod === "200" && weather_data.list) {
-                const current_weather = weather_data.list[0];
-                const cloud_quantity = current_weather.clouds.all;
-                const current_icon = weather_data.list[0].weather[0].icon;
-                const daily_weather = weather_data.list.filter(item => item.dt_txt.includes("12:00:00"));
-                const current_date = new Date(current_weather.dt * 1000);
-                const current_day_en_time = current_date.toLocaleDateString('en-US',
-                    { weekday: 'long', hour: '2-digit', minute: '2-digit', hour12: false });
-                let cloud_description = "";
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`City not found. HTTP status: ${response.status}`);
+        }
+        const weather_data = await response.json();
+        if (weather_data.cod === "200" && weather_data.list) {
+            const current_weather = weather_data.list[0];
+            
+            const utc_date = new Date(current_weather.dt * 1000);
 
-                if (cloud_quantity > 80) {
-                    cloud_description = "many clouds";
-                } else if (cloud_quantity > 20) {
-                    cloud_description = "normal clouds";
-                } else {
-                    cloud_description = "few clouds";
-                }
+            const city_date = new Date(utc_date.getTime() + weather_data.city.timezone * 1000);
 
-                daily_weather.slice(1, 6).forEach(day => {
-                    const date = new Date(day.dt * 1000);
-                    const day_name = date.toLocaleDateString('en-US', { weekday: 'short' });
-                    const temp_high = day.main.temp_max || day.main.temp;
-                    const temp_low = day.main.temp_min || day.main.temp;
-                    const icon = day.weather[0].icon;
+            const cloud_quantity = current_weather.clouds.all;
+            const current_icon = weather_data.list[0].weather[0].icon;
+            const daily_weather = weather_data.list.filter(item => item.dt_txt.includes("12:00:00"));
 
-                    days.push(day_name);
-                    icons.push(icon);
-                    high_temps.push(temp_high);
-                    low_temps.push(temp_low);
-                });
+            const cityTimezoneOffset = current_weather.timezone; 
+            const current_date = new Date((current_weather.dt + cityTimezoneOffset) * 1000);
+            const current_day_en_time = city_date.toLocaleDateString('en-US',
+                { weekday: 'long', hour: '2-digit', minute: '2-digit', hour12: false });
+            let cloud_description = "";
 
-                return {
-                    city: weather_data.city.name,
-                    current_day_en_time: current_day_en_time,
-                    current_temp: weather_data.list[0].main.temp,
-                    current_icon: current_icon,
-                    humidity: weather_data.list[0].main.humidity,
-                    wind: weather_data.list[0].wind.speed,
-                    cloud_description: cloud_description,
-                    icons: icons,
-                    days: days,
-                    high_temps: high_temps,
-                    low_temps: low_temps
-                };
-
+            if (cloud_quantity > 80) {
+                cloud_description = "many clouds";
+            } else if (cloud_quantity > 20) {
+                cloud_description = "normal clouds";
             } else {
-                throw new Error("Unable to fetch cloud quantity data!");
+                cloud_description = "few clouds";
             }
-        });
+
+            daily_weather.slice(1, 6).forEach(day => {
+                const date = new Date(day.dt * 1000);
+                const day_name = date.toLocaleDateString('en-US', { weekday: 'long' });
+                const temp_high = day.main.temp_max || day.main.temp;
+                const temp_low = day.main.temp_min || day.main.temp;
+                const icon = day.weather[0].icon;
+
+                days.push(day_name);
+                icons.push(icon);
+                high_temps.push(temp_high);
+                low_temps.push(temp_low);
+            });
+
+            return {
+                city: weather_data.city.name,
+                current_day_en_time: current_day_en_time,
+                current_temp: weather_data.list[0].main.temp,
+                current_icon: current_icon,
+                humidity: weather_data.list[0].main.humidity,
+                wind: weather_data.list[0].wind.speed,
+                cloud_description: cloud_description,
+                icons: icons,
+                days: days,
+                high_temps: high_temps,
+                low_temps: low_temps
+            };
+
+        } else {
+            throw new Error("Unable to fetch cloud quantity data!");
+        }
     };
 }
 
@@ -79,8 +84,8 @@ function SetWeatherData() {
         document.getElementById(id).src = `https://openweathermap.org/img/wn/${object_data}@2x.png`;
     }
 
-    this.set_weekly_data = function (days, icons, high_temps, low_temps) {
-        const container = document.querySelector(".five-predictions");
+    this.set_weekly_data = function (container_id, days, icons, high_temps, low_temps) {
+        const container = document.querySelector(container_id);
         container.innerHTML = ``;
 
         days.forEach((day_name, index) => {
@@ -109,15 +114,19 @@ weather_form.addEventListener("submit", async (event) => {
 
         const set_weather = new SetWeatherData();
         set_weather.set_id_data(forecast.city, "city");
+        set_weather.set_id_data(forecast.city, "forecast-city");
         set_weather.set_id_data(forecast.current_temp, "temperature");
-        set_weather.set_id_data(forecast.current_day_en_time, "current-day");
-        set_weather.set_id_data(forecast.humidity, "humidity");
-        set_weather.set_id_data(forecast.wind, "wind");
+        set_weather.set_id_data(forecast.current_day_en_time, "current-day-time");
+        set_weather.set_id_data(forecast.humidity, "humidity-percentage");
+        set_weather.set_id_data(forecast.wind, "wind-speed");
         set_weather.set_id_data(forecast.cloud_description, "clouds-status");
 
-        set_weather.current_day_icon(forecast.current_icon, "icon")
+        set_weather.current_day_icon(forecast.current_icon, "current-icon")
 
-        set_weather.set_weekly_data(forecast.days, forecast.icons, forecast.high_temps, forecast.low_temps);
+        set_weather.set_weekly_data(
+            "#future-forecast", forecast.days, forecast.icons, 
+            forecast.high_temps, forecast.low_temps
+        );
     } catch (error) {
         console.log("Error while fetching data:", error);
     }
